109a4e39a5dae375b4a2807fc404ae08f17a70d1
VelocityKatrajPune <yjoshi02@gmail.com>
2022-04-06 15:39:12 +0530

    added

---
+++ src/intro/Myclass.java
@@ -0,0 +1,12 @@
+package intro;
+
+public class Myclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi This is my 1st Java program");
+		
+
+	}
+
+}
---
+++ src/intro/VaribleStudy.java
@@ -0,0 +1,23 @@
+package intro;
+
+public class VaribleStudy {
+
+	public static void main(String[] args) {
+	
+//		1. Variable declaration 
+//		2. Variable Initialization (Assigning or Inserting value)
+//		3. Usage (Printing/ or any other operations)
+
+		
+		//1.Variable declaration
+		int rollNum;
+		
+		//2.Variable Initialization
+		rollNum=100;
+		
+		//3. Usage (Printing/ or any other operations)
+		System.out.println(rollNum);
+
+	}
+
+}
